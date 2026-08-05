fd797d82f773d2e58f914802eba62d81707c39a7
TaiwanJohn <53088692+john8895@users.noreply.github.com>
2026-08-05 23:19:44 +0800

    fix: tailwind 安裝、proxy 路徑修正、API 資料解析

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://data.moenv.gov.tw',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/moenv/, ''),
       },
     },
   },
